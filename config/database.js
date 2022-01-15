4df6de6d15169c430827a0d2ce5b4a666737127d
Arsen <inaruto85@gmail.com>
2022-01-15 11:57:30 +0500

    asdas

--- config/database.js
+++ config/database.js
@@ -16,22 +16,22 @@ module.exports = ({ env }) => {
           database: config.database,
           user: config.user,
           password: config.password,
-          ssl: env.bool('DATABASE_SSL', false),
+          // ssl: env.bool('DATABASE_SSL', false),
         },
       },
     }
   }
-    return{
-    connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapiapi'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'n3wvz6'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-    },
-  }
+  //   return{
+  //   connection: {
+  //     client: 'postgres',
+  //     connection: {
+  //       host: env('DATABASE_HOST', '127.0.0.1'),
+  //       port: env.int('DATABASE_PORT', 5432),
+  //       database: env('DATABASE_NAME', 'strapiapi'),
+  //       user: env('DATABASE_USERNAME', 'postgres'),
+  //       password: env('DATABASE_PASSWORD', 'n3wvz6'),
+  //       ssl: env.bool('DATABASE_SSL', false),
+  //     },
+  //   },
+  // }
 };
